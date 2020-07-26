3fce212b64a0fff33abc3235bb2e815a1fbb0425
Your Name <you@example.com>
2020-07-26 16:37:29 +0500

    Test view vertical alignment

--- modules/loadroute/src/main/java/com/esv/tripplanner/loadroute/fragments/LoadRouteFragment.kt
+++ modules/loadroute/src/main/java/com/esv/tripplanner/loadroute/fragments/LoadRouteFragment.kt
@@ -9,6 +9,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.esv.tripplanner.core_api.data.entities.PointOfInterestVisitPlan
+import com.esv.tripplanner.core_api.data.entities.Trip
+import com.esv.tripplanner.core_api.data.entities.relation_classes.TripVisitPlansRelation
 import com.esv.tripplanner.core_api.di.AppWithFacade
 import com.esv.tripplanner.core_api.ui.InjectableFragment
 import com.esv.tripplanner.core_api.viewModelFactories.CustomViewModelProviderFactory
@@ -19,6 +22,7 @@ import com.esv.tripplanner.loadroute.viewModels.LoadRouteViewModel
 import kotlinx.android.synthetic.main.load_route_fragment.*
 import kotlinx.android.synthetic.main.load_route_fragment.view.*
 import javax.inject.Inject
+import kotlin.random.Random
 
 
 class LoadRouteFragment : InjectableFragment() {
@@ -58,31 +62,54 @@ class LoadRouteFragment : InjectableFragment() {
         trips.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
 
         val dividerItemDecoration = DividerItemDecoration(
-            trips.getContext(),
+            context,
             RecyclerView.VERTICAL
         )
         trips.addItemDecoration(dividerItemDecoration)
+
+
+        // add dummy test data
         val adapter =
             TripItemsAdapter(
                 requireActivity().application,
-                arrayListOf(),
+                getDummyData(),
                 navComponent.provideNavigator()
             )
         trips.trips.adapter = adapter
-
-        viewModel.trips.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                if (it.isNotEmpty())
-                    adapter.setTrips(it)
-            }
-        })
 
         super.onViewCreated(view, savedInstanceState)
     }
 
     override fun performInjection() {
-        LoadRouteComponent.createComponent((requireActivity().application as AppWithFacade)
-            .getProvidersFacade())
+        LoadRouteComponent.createComponent(
+            (requireActivity().application as AppWithFacade)
+                .getProvidersFacade()
+        )
             .inject(this)
     }
+
+
+    private fun getDummyData():MutableList<TripVisitPlansRelation>{
+        val items: MutableList<TripVisitPlansRelation> = mutableListOf()
+        items.add(getDummyTripRelation(5))
+        items.add(getDummyTripRelation(15))
+        items.add(getDummyTripRelation(50))
+        items.add(getDummyTripRelation(150))
+        items.add(getDummyTripRelation(250))
+        return items
+    }
+
+    private fun getDummyTripRelation(poiCount: Int): TripVisitPlansRelation{
+        val dummyTrip = Trip("My trip_${Random(0).nextInt()}", "", Math.random() > 0.3)
+        return  TripVisitPlansRelation(dummyTrip, getRandomPoiVisitPlan(poiCount))
+    }
+
+    private fun getRandomPoiVisitPlan(count: Int): List<PointOfInterestVisitPlan> {
+        val items: MutableList<PointOfInterestVisitPlan> = mutableListOf()
+        for (i in 0 until count) {
+            items.add(PointOfInterestVisitPlan(i, Math.random(), Math.random() > 0.3))
+        }
+        return items
+    }
+
 }
