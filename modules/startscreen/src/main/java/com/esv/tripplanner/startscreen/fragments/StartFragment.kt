aa0c4aa41a76ab546d362536db3a1ba2de7c0be7
Your Name <you@example.com>
2020-07-29 11:17:07 +0500

    Added animations for fragment transactions (via nav_graph), property animation and valueanimations for main screen controls

--- modules/startscreen/src/main/java/com/esv/tripplanner/startscreen/fragments/StartFragment.kt
+++ modules/startscreen/src/main/java/com/esv/tripplanner/startscreen/fragments/StartFragment.kt
@@ -1,23 +1,31 @@
 package com.esv.tripplanner.startscreen.fragments
 
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.esv.tripplanner.core_api.di.AppWithFacade
-import com.esv.tripplanner.core_api.navigation.INavigationProvider
-import com.esv.tripplanner.core_api.repositories.ITripRepository
-import com.esv.tripplanner.core_api.ui.InjectableFragment
-import com.esv.tripplanner.core_api.viewModelFactories.CustomViewModelProviderFactory
 import com.esv.tripplanner.startscreen.R
 import com.esv.tripplanner.startscreen.databinding.StartFragmentBinding
 import com.esv.tripplanner.startscreen.di.StartComponent
 import com.esv.tripplanner.startscreen.viewModels.StartFragmentViewModel
+import kotlinx.android.synthetic.main.start_fragment.*
 import javax.inject.Inject
 
+
 class StartFragment : com.esv.tripplanner.core_api.ui.InjectableFragment() {
+
+    val TITLE_INDEX: String ="TITLE_INDEX"
 
     @Inject
     lateinit var repository: com.esv.tripplanner.core_api.repositories.ITripRepository
@@ -50,12 +58,61 @@ class StartFragment : com.esv.tripplanner.core_api.ui.InjectableFragment() {
             }
         ).get(StartFragmentViewModel::class.java)
 
+
+
         binding.viewModel = viewModel
         // Inflate the layout for this fragment
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        animateWelcomeText()
+        runAppTitleAnimation()
+    }
+
+    private fun animateWelcomeText() {
+        welcome_text?.let {
+
+            val destScale = if(it.scaleX > 1f){1f}else{1.5f}
+
+            it.animate().scaleX(destScale).scaleY(destScale)
+                .setInterpolator(OvershootInterpolator())
+                .setDuration(1000)
+                .withEndAction(){animateWelcomeText()}
+                .start()
+        }
+    }
+
+    private fun runAppTitleAnimation(){
+            val textIndexHolder = PropertyValuesHolder.ofInt(TITLE_INDEX, *(0..app_title.text.length).toList().toIntArray())
+            val srcTitleText = app_title.text
+            ValueAnimator().apply {
+                setValues(textIndexHolder)
+                duration = 1500
+                repeatCount =  ValueAnimator.INFINITE
+                repeatMode = ValueAnimator.REVERSE
+                addUpdateListener {
+                    val index = it.getAnimatedValue(TITLE_INDEX) as Int
+                    val spannableText = SpannableString(srcTitleText)
+                    spannableText.setSpan(
+                        TextAppearanceSpan(context, com.esv.tripplanner.shared_ui.R.style.TitleSpannableTextStyle),
+                        index,
+                        if(index != srcTitleText.length) {index+1}else{index},
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                    spannableText.setSpan(
+                        ForegroundColorSpan(resources.getColor(R.color.design_default_color_error)),
+                        index,
+                        if(index != srcTitleText.length) {index+1}else{index},
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                    app_title?.let {
+                        app_title.setText(spannableText, TextView.BufferType.SPANNABLE)
+                    }
+                }
+            }.start()
+    }
 
     override fun performInjection() {
         StartComponent.createComponent((requireActivity().application as AppWithFacade)
